aac096467428d0943464ee6ff909c3404d184d76
Redstonerayy <antonpaulr@protonmail.com>
2023-12-14 13:19:27 +0100

    remove unused

--- src/server/util.cpp
+++ src/server/util.cpp
@@ -12,9 +12,4 @@ unsigned int get_logical_cpus()
     return logical_cpus != 0 ? logical_cpus : 1; // return 1 if can't detect count
 }
 
-void sleep_miliseconds(int ms)
-{
-    std::this_thread::sleep_for(std::chrono::milliseconds(ms));
-}
-
 }
